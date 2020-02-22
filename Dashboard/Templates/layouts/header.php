0b94694903fff24d29f50f9b3238dedabff7db26
Eugene <goldmikki@gmail.com>
2020-02-22 17:30:51 +0200

    View logo to page header
    Issue https://github.com/eugene-sukhodolskiy/dashboard/issues/10

--- Dashboard/Templates/layouts/header.php
+++ Dashboard/Templates/layouts/header.php
@@ -1,12 +1,13 @@
 <header class="header">
 	<div class="row">
 		<div class="col-6">
-			<h1 class="page-title">Total: <?= count($projects) ?></h1>
+			<img src="/Dashboard/Resources/imgs/logo.png" class="logo">
 		</div>
 		<div class="col-6">
 			<!-- <form method="post" action="/form.php" class="create-new-project">
 				<input type="text" name="create" class="inp create-project" placeholder="Create new project">
 			</form> -->
+			<h3 class="total">Total: <?= count($projects) ?></h3>
 		</div>
 	</div>
 </header>
