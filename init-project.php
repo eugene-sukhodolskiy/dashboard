e6e334e3749868f88ffa47aadbdd4e4dfd08981c
Eugene <goldmikki@gmail.com>
2020-07-01 23:12:56 +0300

    Make small script for fast creating project.json file
    Issue https://github.com/eugene-sukhodolskiy/dashboard/issues/27

---
+++ init-project.php
@@ -0,0 +1,45 @@
+<?php
+
+function collect_data(){
+	$data = [];
+	$questions = [
+		'Project name:' => 'name',
+		'Version:' => 'ver',
+		'Author name:' => 'author',
+		'Tags:' => 'tags',
+		'Project type:' => 'type'
+	];
+
+	foreach ($questions as $q => $prop_name) {
+		$data[$prop_name] = readline($q . ' ');
+	}
+
+	$data['tags'] = array_map(function($item){
+		return trim($item);
+	}, explode(',', $data['tags']));
+
+	if($data['ver'] == "\n" or $data['ver'] == ''){
+		$data['ver'] = '0.1';
+	}
+
+	$data['status'] = "open";
+	$data['project_color'] = null;
+	$data['git_url'] = null;
+	$data['release_url'] = null;
+
+	return $data;
+}
+
+function create_json($data){
+	$json = json_encode($data, JSON_PRETTY_PRINT);
+	$dir = readline("Path to root dir of project: ");
+	if($dir == "\n" or $dir == ""){
+		$dir = getcwd();
+	}
+
+	$dir .= '/project.json';
+	return file_put_contents($dir, $json);
+}
+
+$data = collect_data();
+create_json($data);
