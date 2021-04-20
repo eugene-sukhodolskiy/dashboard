7f45e80f5ebaf6980415af7a4c6828e0c418e610
Eugene <goldmikki@gmail.com>
2021-04-20 17:37:03 +0300

    Bug with empty fields in project description
    Issue https://github.com/eugene-sukhodolskiy/dashboard/issues/67

--- Dashboard/Templates/blocks/project/links.php
+++ Dashboard/Templates/blocks/project/links.php
@@ -1,21 +1,17 @@
-<div class="line links-container">
-	<span class="heading">
-		<span class="mdi mdi-link"></span>
-		Links:
-	</span>
-	<table class="links">
-		<tbody>
-			<?php foreach ($links as $name => $link): ?>
-				<? if(!strlen($link)) continue; ?>
-				<tr>
-					<th>
-						<?= ucfirst(str_replace(['-', '_', '+'], ' ', trim($name))) ?>
-					</th>
-					<td>
-						<a href="<?= $link ?>" target="_blank"><?= $link ?></a>
-					</td>
-				</tr>
-			<?php endforeach ?>
-		</tbody>
-	</table>
-</div>
+<? $rendered_links = $this -> join('blocks/project/links.list', [
+	'links' => $links
+]) ?>
+
+<? if(strlen(trim($rendered_links))): ?>
+	<div class="line links-container">
+		<span class="heading">
+			<span class="mdi mdi-link"></span>
+			Links:
+		</span>
+		<table class="links">
+			<tbody>
+				<?= $rendered_links ?>
+			</tbody>
+		</table>
+	</div>
+<? endif ?>
--- Dashboard/Templates/blocks/project/tags.php
+++ Dashboard/Templates/blocks/project/tags.php
@@ -1,11 +1,18 @@
-<div class="project-card-info">
-	<span class="heading">
-		<span class="mdi mdi-tag"></span>
-		Tags:
-	</span>
-	<? if(isset($project['project']['tags'])): ?>
-		<? foreach($project['project']['tags'] as $tag): ?>
-			<a href="#" class="tag"><?= $tag ?></a>
-		<? endforeach ?>
-	<? endif ?>
-</div>
+<? if(
+	isset($project['project']['tags']) 
+	and is_array($project['project']['tags']) 
+	and count($project['project']['tags'])): ?>
+
+	<div class="project-card-info">
+		<span class="heading">
+			<span class="mdi mdi-tag"></span>
+			Tags:
+		</span>
+		<? if(isset($project['project']['tags'])): ?>
+			<? foreach($project['project']['tags'] as $tag): ?>
+				<a href="#" class="tag"><?= $tag ?></a>
+			<? endforeach ?>
+		<? endif ?>
+	</div>
+	
+<? endif ?>
